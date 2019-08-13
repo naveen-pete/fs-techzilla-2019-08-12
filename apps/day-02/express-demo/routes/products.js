cfe8e9ee1daf390854a38f72585a8adaa6b69c35
naveen-pete <naveen.shenoy.blr@gmail.com>
2019-08-13 21:04:49 +0530

    commit refactored code into the repo

--- apps/day-02/express-demo/routes/products.js
+++ apps/day-02/express-demo/routes/products.js
@@ -8,78 +8,77 @@ let products = [
   { id: 2, name: 'Samsung Galaxy S10', price: 65000 }
 ];
 
-router.get('/', (req, res) => {
-  res.json(products);
-});
-
-router.post('/', (req, res) => {
-  const result = validateProduct(req.body);
-  if (result.error) {
-    return res.status(400).json({ message: result.error.details[0].message })
-  }
-
-  const newProduct = { ...req.body, id: products[products.length - 1].id + 1 };
-  products.push(newProduct);
-  res.json(newProduct);
-});
-
-router.get('/:id', (req, res) => {
-  const product = products.find(p => p.id === parseInt(req.params.id));
-  if (!product) {
-    return res.status(404).json({ message: 'Product does not exist!' });
-  }
-
-  res.json(product);
-});
-
-router.put('/:id', (req, res) => {
-  const product = products.find(p => p.id === parseInt(req.params.id));
-  if (!product) {
-    return res.status(404).json({ message: 'Product does not exist!' });
-  }
-
-  product.name = req.body.name;
-  product.price = req.body.price;
-
-  res.json(product);
-});
-
-router.delete('/:id', (req, res) => {
-  // #1
-  // let index = 0;
-  // const product = products.find((p, i) => {
-  //   index = i;
-  //   return p.id === parseInt(req.params.id);
-  // });
-
-  // if (!product) {
-  //   return res.status(404).json({ message: 'Product does not exist!' });
-  // }
-  // products.splice(index, 1);
-  // res.json({message: 'Product deleted successfully!'});
-
-  // #2
-  // const index = products.findIndex(p => p.id === parseInt(req.params.id));
-  // products.splice(index, 1);
-  // res.json({message: 'Product deleted successfully!'});
-
-  // #3
-  const product = products.find(p => p.id === parseInt(req.params.id));
-  if (!product) {
-    return res.status(404).json({ message: 'Product does not exist!' });
-  }
-
-  products = products.filter(p => p.id !== parseInt(req.params.id));
-  res.json({ message: 'Product deleted successfully!' });
-});
-
 const validateProduct = product => {
   const schema = Joi.object().keys({
     name: Joi.string().required(),
     price: Joi.number().required()
   });
 
-  return Joi.validate(product, schema);
+  return Joi.validate(product, schema, { convert: false });
 }
+
+router.route('/')
+  .get((req, res) => {
+    res.json(products);
+  })
+  .post((req, res) => {
+    const result = validateProduct(req.body);
+    if (result.error) {
+      return res.status(400).json({ message: result.error.details[0].message })
+    }
+
+    const newProduct = { ...req.body, id: products[products.length - 1].id + 1 };
+    products.push(newProduct);
+    res.json(newProduct);
+  });
+
+router.route('/:id')
+  .get((req, res) => {
+    const product = products.find(p => p.id === parseInt(req.params.id));
+    if (!product) {
+      return res.status(404).json({ message: 'Product does not exist!' });
+    }
+
+    res.json(product);
+  })
+  .put((req, res) => {
+    const product = products.find(p => p.id === parseInt(req.params.id));
+    if (!product) {
+      return res.status(404).json({ message: 'Product does not exist!' });
+    }
+
+    product.name = req.body.name;
+    product.price = req.body.price;
+
+    res.json(product);
+  })
+  .delete((req, res) => {
+    // #1
+    // let index = 0;
+    // const product = products.find((p, i) => {
+    //   index = i;
+    //   return p.id === parseInt(req.params.id);
+    // });
+
+    // if (!product) {
+    //   return res.status(404).json({ message: 'Product does not exist!' });
+    // }
+    // products.splice(index, 1);
+    // res.json({message: 'Product deleted successfully!'});
+
+    // #2
+    // const index = products.findIndex(p => p.id === parseInt(req.params.id));
+    // products.splice(index, 1);
+    // res.json({message: 'Product deleted successfully!'});
+
+    // #3
+    const product = products.find(p => p.id === parseInt(req.params.id));
+    if (!product) {
+      return res.status(404).json({ message: 'Product does not exist!' });
+    }
+
+    products = products.filter(p => p.id !== parseInt(req.params.id));
+    res.json({ message: 'Product deleted successfully!' });
+  });
 
 module.exports = router;
